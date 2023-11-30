b0235da5521ed69bbe40dc2ed3a7d39cd4551b6e
samsa <samsam.deschamps@gmail.com>
2023-11-30 11:36:34 +0100

    Add project files.

---
+++ MainForm.cpp
@@ -0,0 +1,13 @@
+#include "pch.h"
+#include "MainForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+int __clrcall WinMain(array<String^>^ args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	IHM::MainForm mainForm;
+	Application::Run(% mainForm);
+}
