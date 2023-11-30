b8b7518cb29ae591da71d32540faffed880b9ec5
Sunslihgt <samsam.deschamps@gmail.com>
2023-11-30 14:06:57 +0100

    Ajout des boutons sur le MainForm. Création de EmployeeList. Lancement de EmployeeList depuis le MainForm.

--- MainForm.h
+++ MainForm.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "EmployeeList.h"
+
 namespace IHM {
 
 	using namespace System;
@@ -12,11 +14,9 @@ namespace IHM {
 	/// <summary>
 	/// Summary for MainForm
 	/// </summary>
-	public ref class MainForm : public System::Windows::Forms::Form
-	{
+	public ref class MainForm : public System::Windows::Forms::Form {
 	public:
-		MainForm(void)
-		{
+		MainForm(void) {
 			InitializeComponent();
 			//
 			//TODO: Add the constructor code here
@@ -27,57 +27,163 @@ namespace IHM {
 		/// <summary>
 		/// Clean up any resources being used.
 		/// </summary>
-		~MainForm()
-		{
-			if (components)
-			{
+		~MainForm() {
+			if (components) {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ lblTitle;
+	private: System::Windows::Forms::Button^ btnClient;
+	private: System::Windows::Forms::Button^ btnItem;
+	private: System::Windows::Forms::Button^ btnPurchase;
+	private: System::Windows::Forms::Button^ btnStatistics;
+	private: System::Windows::Forms::Label^ lblSuperior;
+	private: System::Windows::Forms::Label^ lblEmployee;
+	private: System::Windows::Forms::Button^ btnEmployee;
+
 	protected:
 
 	private:
 		/// <summary>
 		/// Required designer variable.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+		System::ComponentModel::Container^ components;
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
 		/// Required method for Designer support - do not modify
 		/// the contents of this method with the code editor.
 		/// </summary>
-		void InitializeComponent(void)
-		{
-			this->label1 = (gcnew System::Windows::Forms::Label());
+		void InitializeComponent(void) {
+			this->lblTitle = (gcnew System::Windows::Forms::Label());
+			this->lblSuperior = (gcnew System::Windows::Forms::Label());
+			this->lblEmployee = (gcnew System::Windows::Forms::Label());
+			this->btnEmployee = (gcnew System::Windows::Forms::Button());
+			this->btnClient = (gcnew System::Windows::Forms::Button());
+			this->btnItem = (gcnew System::Windows::Forms::Button());
+			this->btnPurchase = (gcnew System::Windows::Forms::Button());
+			this->btnStatistics = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
-			// label1
+			// lblTitle
 			// 
-			this->label1->AutoSize = true;
-			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->lblTitle->Anchor = static_cast<System::Windows::Forms::AnchorStyles>(((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->lblTitle->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label1->Location = System::Drawing::Point(84, 9);
-			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(120, 25);
-			this->label1->TabIndex = 0;
-			this->label1->Text = L"Projet POO";
-			this->label1->Click += gcnew System::EventHandler(this, &MainForm::label1_Click);
+			this->lblTitle->Location = System::Drawing::Point(12, 9);
+			this->lblTitle->Name = L"lblTitle";
+			this->lblTitle->Size = System::Drawing::Size(364, 25);
+			this->lblTitle->TabIndex = 0;
+			this->lblTitle->Text = L"Projet POO";
+			this->lblTitle->TextAlign = System::Drawing::ContentAlignment::TopCenter;
+			// 
+			// lblSuperior
+			// 
+			this->lblSuperior->Anchor = static_cast<System::Windows::Forms::AnchorStyles>(((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->lblSuperior->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->lblSuperior->Location = System::Drawing::Point(13, 59);
+			this->lblSuperior->Margin = System::Windows::Forms::Padding(3, 25, 3, 0);
+			this->lblSuperior->Name = L"lblSuperior";
+			this->lblSuperior->Size = System::Drawing::Size(359, 23);
+			this->lblSuperior->TabIndex = 6;
+			this->lblSuperior->Text = L"Supérieur";
+			this->lblSuperior->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
+			// 
+			// lblEmployee
+			// 
+			this->lblEmployee->Anchor = static_cast<System::Windows::Forms::AnchorStyles>(((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->lblEmployee->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->lblEmployee->Location = System::Drawing::Point(13, 136);
+			this->lblEmployee->Margin = System::Windows::Forms::Padding(3, 25, 3, 0);
+			this->lblEmployee->Name = L"lblEmployee";
+			this->lblEmployee->Size = System::Drawing::Size(359, 23);
+			this->lblEmployee->TabIndex = 7;
+			this->lblEmployee->Text = L"Employé";
+			this->lblEmployee->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
+			// 
+			// btnEmployee
+			// 
+			this->btnEmployee->Anchor = System::Windows::Forms::AnchorStyles::Top;
+			this->btnEmployee->Location = System::Drawing::Point(154, 85);
+			this->btnEmployee->Name = L"btnEmployee";
+			this->btnEmployee->Size = System::Drawing::Size(75, 23);
+			this->btnEmployee->TabIndex = 1;
+			this->btnEmployee->Text = L"Employés";
+			this->btnEmployee->UseVisualStyleBackColor = true;
+			this->btnEmployee->Click += gcnew System::EventHandler(this, &MainForm::btnEmployeeClick);
+
+			// 
+			// btnClient
+			// 
+			this->btnClient->Anchor = System::Windows::Forms::AnchorStyles::Top;
+			this->btnClient->Location = System::Drawing::Point(154, 162);
+			this->btnClient->Name = L"btnClient";
+			this->btnClient->Size = System::Drawing::Size(75, 23);
+			this->btnClient->TabIndex = 2;
+			this->btnClient->Text = L"Clients";
+			this->btnClient->UseVisualStyleBackColor = true;
+			// 
+			// btnItem
+			// 
+			this->btnItem->Anchor = System::Windows::Forms::AnchorStyles::Top;
+			this->btnItem->Location = System::Drawing::Point(154, 191);
+			this->btnItem->Name = L"btnItem";
+			this->btnItem->Size = System::Drawing::Size(75, 23);
+			this->btnItem->TabIndex = 3;
+			this->btnItem->Text = L"Produits";
+			this->btnItem->UseVisualStyleBackColor = true;
+			// 
+			// btnPurchase
+			// 
+			this->btnPurchase->Anchor = System::Windows::Forms::AnchorStyles::Top;
+			this->btnPurchase->Location = System::Drawing::Point(154, 220);
+			this->btnPurchase->Name = L"btnPurchase";
+			this->btnPurchase->Size = System::Drawing::Size(75, 23);
+			this->btnPurchase->TabIndex = 4;
+			this->btnPurchase->Text = L"Commandes";
+			this->btnPurchase->UseVisualStyleBackColor = true;
+			// 
+			// btnStatistics
+			// 
+			this->btnStatistics->Anchor = System::Windows::Forms::AnchorStyles::Top;
+			this->btnStatistics->Location = System::Drawing::Point(154, 249);
+			this->btnStatistics->Name = L"btnStatistics";
+			this->btnStatistics->Size = System::Drawing::Size(75, 23);
+			this->btnStatistics->TabIndex = 5;
+			this->btnStatistics->Text = L"Statistiques";
+			this->btnStatistics->UseVisualStyleBackColor = true;
 			// 
 			// MainForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(284, 261);
-			this->Controls->Add(this->label1);
+			this->ClientSize = System::Drawing::Size(388, 407);
+			this->Controls->Add(this->lblEmployee);
+			this->Controls->Add(this->lblSuperior);
+			this->Controls->Add(this->btnStatistics);
+			this->Controls->Add(this->btnPurchase);
+			this->Controls->Add(this->btnItem);
+			this->Controls->Add(this->btnClient);
+			this->Controls->Add(this->btnEmployee);
+			this->Controls->Add(this->lblTitle);
 			this->Name = L"MainForm";
-			this->Text = L"MainForm";
+			this->Text = L"Projet POO - Groupe 1";
+			this->Load += gcnew System::EventHandler(this, &MainForm::mainFormLoad);
 			this->ResumeLayout(false);
-			this->PerformLayout();
-
 		}
 #pragma endregion
-	private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {}
+		private: System::Void mainFormLoad(System::Object^ sender, System::EventArgs^ e) {
+
+		}
+
+		private: System::Void btnEmployeeClick(System::Object^ sender, System::EventArgs^ e) {
+			IHM::EmployeeList employeeList;
+			employeeList.ShowDialog();
+		}
 	};
 }
